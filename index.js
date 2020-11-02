09aec6665df4df346b57b3488a7760afae7cd421
wakennedy <alexkenned@gmail.com>
2020-11-02 13:28:19 -0800

    employ dotenv, and mongoose.connect

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+//Connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to db!")
+);
 
 //Import Routes
 const authRoute = require("./routes/auth");
