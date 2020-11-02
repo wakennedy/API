2b3b1b09cd636a228d8faa487f27f61a82a9493f
wakennedy <alexkenned@gmail.com>
2020-11-02 14:14:45 -0800

    separate validation file

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,28 @@
 const router = require("express").Router();
 const User = require("../model/User");
-
-//Validation
-const Joi = require("@hapi/joi");
-
-//define the object to validate
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
+const { registerValidation, loginValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
   //validate data before we create user
-  const { error } = schema.validate(req.body);
-
+  const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  //check for uniqueness
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already Exists");
+
+  //create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
   try {
+    //save
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (err) {
+    //catch error
     res.status(400).send(err);
   }
 });
